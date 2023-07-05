06fc3c2ac1c57b83d9d8f1309c7c66654b20f993
alienwebvision <alienwebvision@gmail.com>
2023-07-05 07:56:19 -0300

    Atualização da migrate usrs

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,10 +14,15 @@ return new class extends Migration
             // $table->id();
             $table->uuid('id')->primary();
             $table->string('name');
+            $table->string('cpf');
+            $table->string('rg');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('usuario');
             $table->string('password');
             $table->rememberToken();
+            $table->string('sei');
+            $table->string('image');
             $table->timestamps();
         });
     }
